06aee98b51ca353aa864ed10d502280afd8202ba
wangqinyin <wangqinyincase@163.com>
2023-02-03 14:03:47 +0800

    GettingStarted first commit

---
+++ Source/GettingStarted/Main.cpp
@@ -0,0 +1,15 @@
+#include "glad/glad.h"
+#include "glfw/glfw3.h"
+
+int main()
+{
+	//// 定义函数原型
+	//typedef void (*GL_GENBUFFERS) (GLsizei, GLuint*);
+	//// 找到正确的函数并赋值给函数指针
+	//GL_GENBUFFERS glGenBuffers = (GL_GENBUFFERS)wglGetProcAddress("glGenBuffers");
+	//// 现在函数可以被正常调用了
+	//GLuint buffer;
+	//glGenBuffers(1, &buffer);
+
+	return 0;
+}
